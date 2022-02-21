f6a28a73756c968302a2b7ee3113a5cf97dc5671
vinstex <ngunzuluvincent@gmail.com>
2022-02-21 17:17:20 +0300

    CRUD

--- app/src/main/java/com/vinstex/exceltest/MainActivity.kt
+++ app/src/main/java/com/vinstex/exceltest/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.vinstex.exceltest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.*
@@ -17,7 +17,11 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.textView)
         val ourWB = createWorkbook()
         createExcelFile(ourWB)
-        compute()
+        updateCell()
+        // deleteSheet()
+        deleteRow()
+        //compute()
+        //deleteColumn()
 
     }
 
@@ -27,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         //Creating a sheet called "statSheet" inside the workbook and then add data to it
         val sheet: Sheet = ourWorkbook.createSheet("statSheet")
+        ourWorkbook.createSheet("testSheet")
         addData(sheet)
 
         return ourWorkbook
@@ -42,29 +47,34 @@ class MainActivity : AppCompatActivity() {
         val row5 = sheet.createRow(4)
         val row6 = sheet.createRow(5)
         val row7 = sheet.createRow(6)
+        val row8 = sheet.createRow(7)
 
 
         //Adding data to each  cell
-        createCell(row1, 0, "Mike")
-        createCell(row1, 1, "470")
+        createCell(row1, 0, "Name")
+        createCell(row1, 1, "Score")
 
-        createCell(row2, 0, "Montessori")
-        createCell(row2, 1, "460")
+        createCell(row2, 0, "Mike")
+        createCell(row2, 1, "470")
 
-        createCell(row3, 0, "Sandra")
-        createCell(row3, 1, "380")
+        createCell(row3, 0, "Montessori")
+        createCell(row3, 1, "460")
 
-        createCell(row4, 0, "Moringa")
-        createCell(row4, 1, "300")
+        createCell(row4, 0, "Sandra")
+        createCell(row4, 1, "380")
 
-        createCell(row5, 0, "Torres")
-        createCell(row5, 1, "270")
+        createCell(row5, 0, "Moringa")
+        createCell(row5, 1, "300")
 
-        createCell(row6, 0, "McGee")
-        createCell(row6, 1, "420")
+        createCell(row6, 0, "Torres")
+        createCell(row6, 1, "270")
 
-        createCell(row7, 0, "Gibbs")
-        createCell(row7, 1, "510")
+        createCell(row7, 0, "McGee")
+        createCell(row7, 1, "420")
+
+        createCell(row8, 0, "Gibbs")
+        createCell(row8, 1, "510")
+
     }
 
     //function for creating a cell.
@@ -107,9 +117,9 @@ class MainActivity : AppCompatActivity() {
             //Check if file exists or not
             if (it.exists()) {
                 //check the file in the directory called "test.xlsx"
-                var createdExcel = File(ourAppFileDirectory, "test.xlsx")
+                val retrievedExcel = File(ourAppFileDirectory, "test.xlsx")
                 //return the file
-                return createdExcel
+                return retrievedExcel
             }
         }
         return null
@@ -172,7 +182,7 @@ class MainActivity : AppCompatActivity() {
             var stdDeviationTo2dp = String.format("%.2f", stdDeviation)
             var varianceTo2dp: String = String.format("%.2f", variance)
 
-            //setting text to the textview
+            //displaying the text to the textview
             textView.setText("From the Spreadsheet, we get these:\n\n MEAN: " + meanTo2dp + "\n VARIANCE: " + varianceTo2dp + "\n STD DEVIATION: " + stdDeviationTo2dp)
         }
 
@@ -199,4 +209,186 @@ class MainActivity : AppCompatActivity() {
         var avgVariance = indexVariance / arrayArg.size
         return avgVariance
     }
+
+    private fun updateCell() {
+        getExcelFile()?.let {
+            try {
+
+                //Reading it as stream
+                val workbookStream = FileInputStream(it)
+
+                //Return the loaded workbook
+                val workbook = WorkbookFactory.create(workbookStream)
+                if (workbook.numberOfSheets > 0) {
+
+                    //Return the first sheet
+                    val sheet = workbook.getSheetAt(0)
+                    //choosing the first row as the headers
+                    var nameHeaderCell = sheet.getRow(0).getCell(0)
+                    var scoreHeaderCell = sheet.getRow(0).getCell(1)
+
+                    //selecting cells to be editted and formatted
+                    var targetCellLabel = sheet.getRow(1).getCell(0)
+                    var targetCellValue = sheet.getRow(1).getCell(1)
+
+                    val font: Font = workbook.createFont()
+                    val headerCellStyle = workbook.createCellStyle()
+                    val targetCellDataStyle = workbook.createCellStyle()
+
+                    //choosing white color and a bold formatting
+                    font.color = IndexedColors.WHITE.index
+                    font.bold = true
+
+                    //applying formatting styles to the cells
+                    headerCellStyle.setAlignment(HorizontalAlignment.LEFT)
+                    headerCellStyle.fillForegroundColor = IndexedColors.RED.index
+                    headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
+                    headerCellStyle.setFont(font);
+                    nameHeaderCell.cellStyle = headerCellStyle
+                    scoreHeaderCell.cellStyle = headerCellStyle
+
+                    targetCellDataStyle.setAlignment(HorizontalAlignment.LEFT)
+                    targetCellValue.cellStyle = targetCellDataStyle
+                    targetCellLabel.cellStyle = targetCellDataStyle
+
+                    //feeding in new values to the selected cells
+                    targetCellLabel.setCellValue("Mitchelle")
+                    targetCellValue.setCellValue(140.0)
+
+                    workbookStream.close()
+
+                    //saving the changes
+                    try {
+                        val fileOut = FileOutputStream(it)
+                        workbook.write(fileOut)
+                        fileOut.close()
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun deleteSheet() {
+        getExcelFile()?.let {
+            try {
+
+                //Reading it as stream
+                val workbookStream = FileInputStream(it)
+
+                //Return the loaded workbook
+                val workbook = WorkbookFactory.create(workbookStream)
+                if (workbook.numberOfSheets > 0) {
+                    //removing the second sheet
+                    workbook.removeSheetAt(1)
+                    workbookStream.close()
+
+                    try {
+                        val fileOut = FileOutputStream(it)
+                        workbook.write(fileOut)
+                        fileOut.close()
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun deleteRow() {
+        getExcelFile()?.let {
+            try {
+                val rowNo = 1
+                //Reading it as stream
+                val workbookStream = FileInputStream(it)
+
+                //Return the loaded workbook
+                val workbook = WorkbookFactory.create(workbookStream)
+                if (workbook.numberOfSheets > 0) {
+
+                    //Return the first sheet
+                    val sheet = workbook.getSheetAt(0)
+
+                    //getting the total number of rows available
+                    val totalNoOfRows = sheet.lastRowNum
+
+                    val targetRow = sheet.getRow(rowNo)
+                    if (targetRow != null) {
+                        sheet.removeRow(targetRow)
+                    }
+
+                    /*excluding the last row, move the cells that come
+                    after the deleted row one step behind*/
+                    if (rowNo >= 0 && rowNo < totalNoOfRows) {
+                        sheet.shiftRows(rowNo + 1, totalNoOfRows, -1)
+                    }
+                    workbookStream.close()
+
+                    try {
+                        val fileOut = FileOutputStream(it)
+                        workbook.write(fileOut)
+                        fileOut.close()
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun deleteColumn() {
+        getExcelFile()?.let {
+            try {
+                val colNo = 0
+                //Reading it as stream
+                val workbookStream = FileInputStream(it)
+
+                //Return the loaded workbook
+                val workbook = WorkbookFactory.create(workbookStream)
+                if (workbook.numberOfSheets > 0) {
+
+                    //Return the first sheet
+                    val sheet = workbook.getSheetAt(0)
+                    val totalRows = sheet.lastRowNum
+                    val row = sheet.getRow(colNo)
+                    val maxCell = row.lastCellNum.toInt()
+                    if (colNo >= 0 && colNo <= maxCell) {
+                        for (rowNo in 0..totalRows) {
+                            val targetCol: Cell = sheet.getRow(rowNo).getCell(colNo)
+                            if (targetCol != null) {
+                                sheet.getRow(rowNo).removeCell(targetCol);
+                            }
+                        }
+                    }
+                    workbookStream.close()
+
+                    try {
+                        val fileOut = FileOutputStream(it)
+                        workbook.write(fileOut)
+                        fileOut.close()
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
